012d355854ae4442b9d31a0037841b3e25039247
ruhaan-anand <73781091+ruhaan-anand@users.noreply.github.com>
2021-03-19 11:30:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project")
+var f_name = "Ruhaan"
+var l_name = "Anand"
+var full_name = f_name+" "+l_name
+console.log(full_name)
+var n1 = 354
+var n2 = 30
+var sum = n1+n2
+var difference = n1-n2
+var product = n1*n2
+var quotient = n1/n2
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(quotient)
